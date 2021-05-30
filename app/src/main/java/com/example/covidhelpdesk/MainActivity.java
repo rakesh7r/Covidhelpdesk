7ca7e6284b2cde43faf1bff2848e216fb4987860
rakesh2002 <rakeshgandla200@gmail.com>
2021-05-30 13:56:46 +0530

    initial

--- app/src/main/java/com/example/covidhelpdesk/MainActivity.java
+++ app/src/main/java/com/example/covidhelpdesk/MainActivity.java
@@ -1,18 +1,48 @@
 package com.example.covidhelpdesk;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
 public class MainActivity extends AppCompatActivity {
     private WebView mywebView;
+    public FusedLocationProviderClient location;
+    public Button linkButton;
+    private View view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        location = LocationServices.getFusedLocationProviderClient(this);
+        if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){}
+        else
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+
+        linkButton = (Button)findViewById(R.id.linkbtn);
+        linkButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                String url = "https://covidhelpdesk-ts.web.app/";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
 
         mywebView=(WebView) findViewById(R.id.webview);
         mywebView.setWebViewClient(new WebViewClient());
@@ -20,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings=mywebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
     }
+
     public static class mywebClient extends WebViewClient{
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon){
